71ca323cd6d867f471360ddbac8a78812e9ed9ef
790206 <ryszard.dudko@gmail.com>
2017-08-18 16:51:42 +0200

    Add new files

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//<p id="value"></p>
+var a = prompt('Podaj wartość a');
+var b = prompt('Podaj wartość b');
+var value = (a*a)+(2*a*b)-(b*b);
+alert('Wynik to: ' + value);
+if(value<0)
+{
+	alert('Wynik jest ujemny');
+} 
+
+else alert('Wynik jest dodatni');
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + b + ' is equal to: ' + value);
